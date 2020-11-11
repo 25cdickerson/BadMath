1eb819e3d25d41f5b2505ebc5110b42236ab9add
Christopher Dickerson <christopher.dickers1@sinclair.edu>
2020-11-11 17:08:24 -0500

    Fixed calculation errors. The inputs in the function was changed to floats. The values being entered are converted to floats before being entered into the function.

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -2,18 +2,26 @@
 #include <iomanip>
 using namespace std;
 
-float average(int i1, int i2, int i3)
+float average(float i1, float i2, float i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	float avg = (i1 + i2 + i3) / 3;
+	return avg;
 }
-
+#include <string>
 int main()
 {
-	int n1 =0 , n2 = 0, n3 = 0;
+	int n1 = 0 , n2 = 0, n3 = 0;
+	string temp;
 	cout << "This program calculates the average of three numbers." << endl;
-	cout << "First number: "; cin >> n1;
-	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "First number: "; 
+	cin >> temp;
+	n1 = stof(temp);
+	cout << "Second number: ";
+	cin >> temp;
+	n2 = stof(temp);
+	cout << "Third number: ";
+	cin >> temp;
+	n3 = stof(temp);
 
 	float a = average(n1, n2, n3);
 
